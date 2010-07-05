21d16227a84bf1aa81e4a4e4606c33bce9872951
Diederick Lawson <webmaster@altovista.nl>
2010-07-05 23:08:07 +0200

    stubs

--- src/tween.js
+++ src/tween.js
@@ -6,37 +6,37 @@
 canvaslib.Tween = {
   _tweens: [],
   _initTime: new Date().getTime(),
-  
+
   /**
    * Tweens an object to the given location, alpha or whatever
    */
-  to: function(obj, duration, toParams, options) {    
+  to: function(obj, duration, toParams, options) {
     // create new tween
     var tween = new canvaslib.TweenCommand(obj, toParams);
-    
+
     // set all options
     tween.duration = duration;
-    
+
     if(options) {
       tween.onComplete = options.onComplete ? options.onComplete : null;
       tween.delay = options.delay ? options.delay : 0;
       tween.ease = options.ease ? options.ease : canvaslib.EaseDefault;
     }
-    
+
     tween.finished = false;
-    
+
     // push to tween stack
     this._tweens.push(tween);
   },
-  
+
   kill: function(obj) {
-    
+
   },
-  
+
   /**
    * Updates all tweens
    */
-  update: function() {    
+  update: function() {
     if(this._tweens.length > 0) {
       var i;
       var time;
@@ -44,25 +44,25 @@ canvaslib.Tween = {
 
       // determine current time
       time = new Date().getTime();
-      
+
       // loop all tweens
-      for(i = 0; i < this._tweens.length; i++) {        
+      for(i = 0; i < this._tweens.length; i++) {
         this._tweens[i].update(time);
-      
+
         if(this._tweens[i].finished) {
           cleanup = true;
           this._tweens[i] = null;
         }
-      }      
-      
+      }
+
       // remove all finished tweens from mem
       if(cleanup) this._cleanup();
     }
   },
-  
+
   _cleanup: function() {
     var i;
-    
+
     for(i = 0; i < this._tweens.length; i++) {
       if(!this._tweens[i]) {
         this._tweens.splice(i, 1);
@@ -75,7 +75,7 @@ canvaslib.Tween = {
 // an individual animation
 canvaslib.TweenCommand = function(obj, params) {
   var property;
-  
+
   this.obj = obj;
   this.startTime = new Date().getTime();
   this.duration = 0;
@@ -85,12 +85,12 @@ canvaslib.TweenCommand = function(obj, params) {
   this.finished = false;
   this.onComplete = null;
   this.startValues = {};
-  
+
   for(property in params) {
     if(obj.hasOwnProperty(property)) {
       // get start value of object
       this.startValues[property] = obj[property];
-      
+
       // calc diff between two values
       this.toParams[property] = this.toParams[property] - obj[property];
     }
@@ -102,18 +102,18 @@ canvaslib.TweenCommand.prototype = {
     var time = updateTime - this.startTime;
     var factor;
     var property;
-    
+
     // are we done?
     if(time >= this.duration) {
       factor = 1;
       this.finished = true;
-      
+
     } else {
       this.finished = false;
       factor = this.ease(time, 0, 1, this.duration);
-      
+
     }
-    
+
     // apply factor to tween values
     for(property in this.toParams) {
       this.obj[property] = this.startValues[property] + (factor * this.toParams[property]);
@@ -124,4 +124,13 @@ canvaslib.TweenCommand.prototype = {
 // easing
 canvaslib.EaseDefault = function(t, b, c, d) {
   return -c * (t /= d) * (t - 2) + b;
+};
+
+canvaslib.ElasticIn = function(t, b, c, d, a, p) {
+};
+
+canvaslib.ElasticOut = function(t, b, c, d, a, p) {
+};
+
+canvaslib.ElasticInOut = function(t, b, c, d, a, p) {
 };
